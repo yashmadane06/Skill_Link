e2b53e9e436fa81e4e6916f40d1b6a1c4531f091
unknown <yashmadanelap06.com>
2025-09-11 09:40:01 +0530

    Set ALLOWED_HOSTS for deployment

--- skilllink/skilllink/settings.py
+++ skilllink/skilllink/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-&7f1um-rj65ryxq666sl&8r-6z-1sckm$kjbvwm#$711dnyxbr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
