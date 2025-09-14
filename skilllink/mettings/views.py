6551a2c7667b125c6c0d9a6337fabf1ec818c7c9
Yash Madane <158011292+yashmadane06@users.noreply.github.com>
2025-09-15 03:39:20 +0530

    Update views.py

--- skilllink/mettings/views.py
+++ skilllink/mettings/views.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from django.db.models import F
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
