eff68079feddae003de4dc8cc892e3266b65ca34
Yash Madane <158011292+yashmadane06@users.noreply.github.com>
2026-01-29 22:02:43 +0530

    updating setting

--- skilllink/skilllink/settings.py
+++ skilllink/skilllink/settings.py
@@ -144,10 +144,9 @@ CHANNEL_LAYERS = {
 #     }
 # else:
 DATABASES = {
-    'default': dj_database_url.parse(
-        'postgres://postgres:Yash@26012006M@db.dkwfssshgeuohlsjhsmy.supabase.co:5432/postgres?sslmode=require'
-    )
+    "default": dj_database_url.parse(os.environ["DATABASE_URL"])
 }
+
 
 
 
